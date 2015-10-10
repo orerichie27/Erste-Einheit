2caebd952ca510d78e9ba12b1bb2a89f114faff2
orerichie27 <Richard.oreskovic@edu.campus02.at>
2015-10-10 15:08:31 +0200

    Erste LV

---
+++ src/Eins.java
@@ -0,0 +1,21 @@
+
+public class Eins {
+
+	public static void main(String[] args) {
+
+		boolean treeFront = true;
+		int numberLeaves =0;
+		char meinZeichen = 'X';
+		
+		
+		
+		System.out.println("Ist ein Baum voraus? "+treeFront);
+		System.out.println();
+		System.out.println("anzahl der Blätter: "+meinZeichen);
+		System.out.println("anzahl der Blätter: "+numberLeaves);
+		
+		
+		
+	}
+
+}
